97ce56893b533e33251bd2c2c6c1483a7547c694
Lisa <lisa.exz@gmail.com>
2019-11-17 20:22:39 +0200

    create controllerPage

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('title', 'Статьи')
 @section('content')
+    @foreach($articles as $article)
+        <h3>{{ $article->name }}</h3>
+        <p>{{ $article->body }}</p>
+    @endforeach
     <p>Тут будут статьи</p>
     <a href="/about">About</a>
     <a href="/articles">Articles</a>
--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,12 @@
 |
 */
 
-$team = [
-    ['name' => 'Hodor', 'position' => 'programmer'],
-    ['name' => 'Joker', 'position' => 'CEO'],
-    ['name' => 'Elvis', 'position' => 'CTO'],
-];
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', function () use ($team) {
-    return view('about', ['team' => $team]);
-});
+Route::get('/about', 'PageController@about');
 Route::get('/articles', function () {
-    return view('articles');
+    $articles = App\Article::all();
+    return view('articles', ['articles' => $articles]);
 });
