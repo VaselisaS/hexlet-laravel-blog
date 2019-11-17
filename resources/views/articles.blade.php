6eaf371622b69df63916ce4ab46d1e244d6b352c
Lisa <lisa.exz@gmail.com>
2019-11-17 13:16:38 +0200

    create model article

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,14 +1,8 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-</head>
-<body>
-<h1>Статьи</h1>
-<a href="/about">About</a>
-<a href="/articles">Articles</a>
-</body>
-</html>
+@extends('layouts.app')
+@section('title', 'Статьи')
+@section('content')
+    <p>Тут будут статьи</p>
+    <a href="/about">About</a>
+    <a href="/articles">Articles</a>
+@endsection
 
